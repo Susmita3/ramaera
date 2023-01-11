7aecd818014130ba0f165c55445784908c0164cc
Susmita Das <susmitad270@gmail.com>
2023-01-11 17:39:46 +0530

    1.0

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,61 +1,128 @@
-import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { ImSearch } from "react-icons/im";
-import { SlArrowDown } from 'react-icons/sl';
+import { SlArrowDown } from "react-icons/sl";
 
 const Nav = () => {
   return (
     <>
-      <nav className="fixed top-0 w-full px-10 py-6 flex items-center flex-row justify-between ">
+      <nav className="fixed top-0 w-full px-10 py-6 flex items-center flex-row justify-between">
         {/* Logo parts start */}
-          <div className="w-1/12">
-            <Link to="/">
-              <img
-                src={require("../Assets/logo.png")}
-                alt="logo"
-                className="w-24"
-              />
-            </Link>
-          </div>
+        <div className="w-1/12">
+          <Link to="/">
+            <img
+              src={require("../Assets/logo.png")}
+              alt="logo"
+              className="w-24"
+            />
+          </Link>
+        </div>
 
         {/* Menu part start */}
-        <div className="flex flex-row items-center justify-between border rounded-2xl w-10/12 h-16 p-2 font-Montserrat backdrop-blur mr-16">
-          <ul className="flex items-center justify-around w-6/12 text-lg ">
+        <div className="flex flex-row items-center justify-between border rounded-2xl w-10/12 h-16 p-2 font-Montserrat backdrop-blur">
+          <ul className="flex items-center justify-around w-6/12 text-lg">
             <li>
-              <NavLink to="/" className="text-white flex items-center gap-2 hover:text-[#E65C00]">
+              <NavLink
+                to="/"
+                className="text-white flex items-center gap-2 hover:text-[#E65C00]"
+              >
                 HOME <SlArrowDown />
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" className="text-white flex items-center gap-2 hover:text-[#E65C00]">
+              <NavLink
+                to="/"
+                className="text-white flex items-center gap-2 hover:text-[#E65C00]"
+              >
                 OUR INDUSTRIES <SlArrowDown />
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" className="text-white flex items-center gap-2 hover:text-[#E65C00]">
+              <NavLink
+                to="/"
+                className="text-white flex items-center gap-2 hover:text-[#E65C00]"
+              >
                 OUR COMPANIES <SlArrowDown />
               </NavLink>
             </li>
           </ul>
-        
+
           <ul className="flex items-center flex-row justify-around w-3/12 text-lg hover:text-[#E65C00]">
             <li>
-              <NavLink to="/" className="text-white text-lg hover:text-[#E65C00]">
+              <NavLink
+                to="/"
+                className="text-white text-lg hover:text-[#E65C00]"
+              >
                 Career
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" className="text-white text-lg hover:text-[#E65C00]">
+              <NavLink
+                to="/"
+                className="text-white text-lg hover:text-[#E65C00]"
+              >
                 Contact
               </NavLink>
             </li>
             <li>
-              <NavLink to="/" className="text-white text-lg hover:text-[#E65C00]">
+              <NavLink
+                to="/"
+                className="text-white text-lg hover:text-[#E65C00]"
+              >
                 <ImSearch />
               </NavLink>
             </li>
           </ul>
         </div>
+
+        {/* Hamburger menu start
+        <nav className="" onClick="onClickMenu()">
+          <div className=""></div>
+          <div className=""></div>
+          <div className=""></div>
+        </nav>
+
+        <ul className="bg-blue-500">
+          <li>
+            <NavLink
+              to="/"
+              className="text-white flex items-center gap-2 hover:text-[#E65C00]"
+            >
+              HOME <SlArrowDown />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/"
+              className="text-white flex items-center gap-2 hover:text-[#E65C00]"
+            >
+              OUR INDUSTRIES <SlArrowDown />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/"
+              className="text-white flex items-center gap-2 hover:text-[#E65C00]"
+            >
+              OUR COMPANIES <SlArrowDown />
+            </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/"
+                className="text-white text-lg hover:text-[#E65C00]"
+              >
+                Career
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/"
+                className="text-white text-lg hover:text-[#E65C00]"
+              >
+                Contact
+              </NavLink>
+            </li>
+        </ul> */}
       </nav>
     </>
   );
--- src/components/Pages/Index.js
+++ src/components/Pages/Index.js
@@ -1,45 +1,25 @@
 import React from "react";
 import SocialNav from "../Navbar/SocialNav";
-import RamaeraHeading from "../Assets/RamaeraHeading.png";
-import MainHeading from "../Assets/MainHeading.png";
-import Polygon from "../Assets/Polygon.png";
-import Tech from "../Assets/Tech.png";
-import Line1 from "../Assets/Line1.png";
-import Line2 from "../Assets/Line2.png";
-import Line3 from "../Assets/Line3.png";
-import Packing from "../Assets/Packing.png";
-import Icon from "../Assets/Icon.png";
-import Icon2 from "../Assets/Icon2.png";
-import Line4 from "../Assets/Line4.png";
-import Line5 from "../Assets/Line5.png";
-import Icon3 from "../Assets/Icon3.png";
-import Fmcg from "../Assets/Fmcg.png";
-import Agri from "../Assets/Agri.png";
-import Icon4 from "../Assets/Icon4.png";
-import ecomrce from "../Assets/ecomrce.png";
-import Line6 from "../Assets/Line6.png";
-import Icon5 from "../Assets/Icon5.png";
-import Polygon2 from "../Assets/Polygon2.png";
 
 const Index = () => {
   return (
     <>
       {/* Hero Section Start */}
-      <div className="bg-hero w-full h-screen flex items-center flex-row justify-center px-10 md:w-full lg:w-full">
+      <div className="bg-hero w-full h-screen flex items-center flex-row justify-center px-10">
         <div className="w-1/12 ">
           <SocialNav />
         </div>
 
-        <main className="text-center flex flex-col items-center justify-center w-11/12 ">
-          <img src={RamaeraHeading} alt="" className="" />
+        <main className="text-center flex flex-col items-center justify-center w-11/12 lg:w-10/12">
+          <img src={require("../Assets/RamaeraHeading.png")} alt="" className="" />
 
-          <p className="text-white text-[44px] font-Montserrat md:text-[25px]">
+          <p className="text-white text-5xl font-Montserrat ">
             An incredible multi-industrial approach oriented <br></br> towards
             financial independence, customer focus <br></br> and serving the
             best quality to the people.
           </p>
 
-          <div className="bg-gradient-to-r from-[#F9D423] to-[#E65C00] text-white hover:text-[#F9D423] rounded-2xl px-6 py-2 mt-5 border-2">
+          <div className="bg-gradient-to-r from-[#F9D423] to-[#E65C00] text-white hover:text-[#F9D423] rounded-2xl px-6 py-2 mt-9 border-2">
             <button className="font-Montserrat text-[25px] ">
               Explore more
             </button>
@@ -50,12 +30,12 @@ const Index = () => {
       {/* second section start */}
       <section className="bg-gradient-to-r from-black to-gray-800 w-full text-white m-0 p-0">
         <div className="flex items-center flex-row ">
-            <img src={Polygon} alt="" className="" />
+            <img src={require("../Assets/Polygon.png")} alt="" className="" />
 
           <div className="text-center ">
-            <img src={MainHeading} alt="" className="" />
-            <p className=" font-Montserrat text-[35px] mt-2 mr-1  ">
-              We are aiming to be a part of the major industrialisation drive{" "}
+            <img src={require("../Assets/MainHeading.png")} alt="" className="" />
+            <p className=" font-Montserrat text-[35px] mt-2">
+              We are aiming to be a part of the major industrialisation drive
               <br></br> in India and embarking our presence in every significant
               <br></br> industrial domain including IT.
             </p>
@@ -64,8 +44,8 @@ const Index = () => {
 
         {/* 1st box */}
         <div className="font-Montserrat ">
-          <img src={Line1} alt="" className="absolute -mt-6" />
-          <img src={Tech} alt="" className="absolute" />
+          <img src={require("../Assets/Line1.png")} alt="" className="absolute -mt-6" />
+          <img src={require("../Assets/Tech.png")} alt="" className="absolute" />
 
           <div className="text-right mr-36">
             <h3 className="text-[55px] font-bold">
@@ -86,13 +66,13 @@ const Index = () => {
               Read more
             </button>
           </div>
-          <img src={Line1} alt="" className="mt-6" />
+          <img src={require("../Assets/Line1.png")} alt="" className="mt-6" />
         </div>
 
         {/* 2nd box */}
         <div className="font-Montserrat">
-          <img src={Line2} alt="" className="" />
-          <img src={Icon} alt="" className="absolute -mt-72" />
+          <img src={require("../Assets/Line2.png")} alt="" className="" />
+          <img src={require("../Assets/Icon.png")} alt="" className="absolute -mt-72" />
 
           <div className="ml-36 mt-5">
             <h3 className="text-transparent bg-clip-text bg-gradient-to-r from-[#FFE259] to-[#FFA751] text-[55px] font-bold ">
@@ -110,14 +90,14 @@ const Index = () => {
               Read more
             </button>
           </div>
-          <img src={Line2} alt="" className="-mt-52" />
+          <img src={require("../Assets/Line2.png")} alt="" className="-mt-52" />
         </div>
 
         {/* 3rd box */}
         <div className="font-Montserrat">
-          <img src={Line3} alt="" className="" />
-          <img src={Packing} alt="" className="absolute -mt-32 " />
-          <img src={Icon2} alt="" className="absolute right-0 -mt-60" />
+          <img src={require("../Assets/Line3.png")} alt="" className="" />
+          <img src={require("../Assets/Packing.png")} alt="" className="absolute -mt-32 " />
+          <img src={require("../Assets/Icon2.png")} alt="" className="absolute right-0 -mt-60" />
 
           <div className="font-bold text-right -mt-60 mr-36">
             <h3 className="text-[55px]">Packaging industry for</h3>
@@ -137,14 +117,14 @@ const Index = () => {
               Read more
             </button>
           </div>
-          <img src={Line3} alt="" className="mt-16" />
+          <img src={require("../Assets/Line3.png")} alt="" className="mt-16" />
         </div>
 
         {/* 4th box */}
         <div className="font-Montserrat">
-          <img src={Line4} alt="" className="" />
-          <img src={Fmcg} alt="" className="absolute right-0 -mt-96" />
-          <img src={Icon3} alt="" className="absolute" />
+          <img src={require("../Assets/Line4.png")} alt="" className="" />
+          <img src={require("../Assets/Fmcg.png")} alt="" className="absolute right-0 -mt-96" />
+          <img src={require("../Assets/Icon3.png")} alt="" className="absolute" />
 
           <div className="font-bold mt-2 ml-36">
             <h3 className="text-transparent bg-clip-text bg-gradient-to-r from-[#F2EFD3] to-[#358D82] text-[55px]">
@@ -161,14 +141,14 @@ const Index = () => {
               Read more
             </button>
           </div>
-          <img src={Line4} alt="" className="-mt-56" />
+          <img src={require("../Assets/Line4.png")} alt="" className="-mt-56" />
         </div>
 
         {/* 5th box */}
         <div className="font-Montserrat">
-          <img src={Line5} alt="" className="" />
-          <img src={Agri} alt="" className="absolute -mt-32 " />
-          <img src={Icon4} alt="" className="absolute right-0 -mt-60" />
+          <img src={require("../Assets/Line5.png")} alt="" className="" />
+          <img src={require("../Assets/Agri.png")} alt="" className="absolute -mt-32 " />
+          <img src={require("../Assets/Icon4.png")} alt="" className="absolute right-0 -mt-60" />
 
           <div className="font-bold text-right -mt-60 mr-36">
             <h3 className="text-[55px]">Agri-business to bestow</h3>
@@ -186,14 +166,14 @@ const Index = () => {
               Read more
             </button>
           </div>
-          <img src={Line5} alt="" className="mt-16" />
+          <img src={require("../Assets/Line5.png")} alt="" className="mt-16" />
         </div>
 
         {/* 6th box */}
         <div className="font-Montserrat">
-          <img src={Line6} alt="" className="" />
-          <img src={ecomrce} alt="" className="absolute right-0 -mt-96" />
-          <img src={Icon5} alt="" className="absolute" />
+          <img src={require("../Assets/Line6.png")} alt="" className="" />
+          <img src={require("../Assets/ecomrce.png")} alt="" className="absolute right-0 -mt-96" />
+          <img src={require("../Assets/Icon5.png")} alt="" className="absolute" />
 
           <div className="font-bold mt-2 ml-36">
             <h3 className="text-transparent bg-clip-text bg-gradient-to-r from-[#B4DFFD] to-[#EFB79B] text-[55px]">
@@ -208,11 +188,11 @@ const Index = () => {
               Read more
             </button>
           </div>
-          <img src={Line4} alt="" className="-mt-56" />
+          <img src={require("../Assets/Line6.png")} alt="" className="-mt-56" />
         </div>
 
         <div className="bg-bg-gradient-to-r from-black to-gray-800 w-full h-screen">
-            <img src={Polygon2} alt="" className="absolute right-0 " />
+            <img src={require("../Assets/Polygon2.png")} alt="" className="absolute right-0 " />
         </div>
       </section>
     </>
